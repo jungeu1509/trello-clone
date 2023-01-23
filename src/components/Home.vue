bdbbffa3fec1bb24d959f5eead8628338d7fdd1b
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2023-01-23 22:48:20 +0900

    섹션 2. 3. API 통신

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,12 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">loading...</div>
+      <div v-else>
+        <div v-for="b in boards" :key="b.id">
+          {{ b }}
+        </div>
+      </div>
       <ul>
         <li><router-link to="/b/1">Board 1</router-link></li>
         <li><router-link to="/b/2">Board 2</router-link></li>
@@ -12,8 +18,24 @@
 </template>
 
 <script>
-export default{
+import {board} from "../api"
 
+export default{
+  data() {
+    return {loading:false, boards: []}
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      board.fetch()
+      .then(data => {
+        this.boards = data
+      })
+    }
+  }
 }
 </script>
 
